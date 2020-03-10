7dbebd76a5eda9e572a6921230d1dd62b60b3cfe
serge <serge.moi@laplateforme.io>
2020-03-10 10:09:13 +0100

    Récupération d'un texte a l'aide de l'événement click

---
+++ jour02/job01/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    </head>
+    <body>
+        <header></header>
+        <main>
+            <section>
+                <article id="citation">
+                    La vie a beaucoup plus d'imagination que nous
+                </article>
+                <button id="button">ok</button>
+            </section>
+        </main>
+        <footer></footer>
+    <script type="text/javascript" src="script.js"></script>
+    </body>
+</html>
